4ff1c473a9c00baa775603b9f68b285bfa0d72f9
masonkarsevar <masonkarsevar@gmail.com>
2020-05-15 09:36:56 -0700

    basic project component styling

--- src/components/projectsComponent.js
+++ src/components/projectsComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { ReactComponent as FishFriends } from "../images/coollogo_com-326361383.svg";
+import { ReactComponent as CoachMe } from "../images/coollogo_com-276021646.svg";
+import { ReactComponent as FoodieFun } from "../images/coollogo_com-279641067.svg";
+import coachMeImage from "../images/coachme1.png";
+import { ReactComponent as ProjectSVG } from "../images/project-diagram-solid.svg";
 
 function ProjectsComponent(props) {
   // console.log("props from app.js", props);
@@ -18,14 +22,39 @@ function ProjectsComponent(props) {
     <div className='projects-container'>
       <fieldset className='project-content'>
         <legend>
-          <FishFriends />
+          <CoachMe />
         </legend>
-        <button onClick={() => handleClickTransform()}>
-          Click to Transform!
-        </button>
-        <button onClick={() => handleClickReTransform()}>
-          Click to Retransform!
-        </button>
+        <div className='image-container'>
+          <img src={coachMeImage} />
+          <div className='project-btn-container'>
+            <button
+              onClick={() => handleClickTransform()}
+              className='project-btn'
+            >
+              Learn More
+            </button>
+            <button
+              onClick={() => handleClickReTransform()}
+              className='project-btn'
+            >
+              Retransform!
+            </button>
+            <div className='project-btn project-btn-div'>
+              <a>
+                <ProjectSVG />
+                <p>Visit App</p>
+              </a>
+            </div>
+            <div className='project-btn project-btn-div'>Github</div>
+          </div>
+        </div>
+        <div className='project-text'>
+          <p>
+            CoachMe is a userfacing web application that gives desired patient
+            management functionality to health coaches and quick health metrics
+            lookup to patients suffering from cronic diseases.
+          </p>
+        </div>
       </fieldset>
       <fieldset className='project-content'>
         <legend>
@@ -34,7 +63,7 @@ function ProjectsComponent(props) {
       </fieldset>
       <fieldset className='project-content'>
         <legend>
-          <FishFriends />
+          <FoodieFun />
         </legend>
       </fieldset>
     </div>
